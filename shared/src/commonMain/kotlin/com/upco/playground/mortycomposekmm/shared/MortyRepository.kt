caaccbc3f57b6e290688512df709bfd24d84ea2e
= <mrchampz@outlook.com>
2021-01-04 17:35:55 -0300

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/upco/playground/mortycomposekmm/shared/MortyRepository.kt
@@ -0,0 +1,66 @@
+package com.upco.playground.mortycomposekmm.shared
+
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.ApolloExperimental
+import com.apollographql.apollo.api.Input
+import com.apollographql.apollo.network.http.ApolloHttpNetworkTransport
+import com.upco.playground.mortycomposekmm.shared.fragment.CharacterDetail
+import com.upco.playground.mortycomposekmm.shared.fragment.EpisodeDetail
+import com.upco.playground.mortycomposekmm.shared.fragment.LocationDetail
+import kotlinx.coroutines.flow.single
+
+@ApolloExperimental
+class MortyRepository {
+    private val apolloClient = ApolloClient(
+        networkTransport = ApolloHttpNetworkTransport(
+            serverUrl = "https://rickandmortyapi.com/graphql",
+            headers = mapOf(
+                "Accept" to "application/json",
+                "Content-Type" to "application/json",
+            )
+        )
+    )
+
+    suspend fun getCharacters(page: Int): GetCharactersQuery.Characters? {
+        val response = apolloClient.query(GetCharactersQuery(Input.optional(page)))
+            .execute()
+            .single()
+        return response.data?.characters
+    }
+
+    suspend fun getCharacter(characterId: String): CharacterDetail? {
+        val response = apolloClient.query(GetCharacterQuery(characterId))
+            .execute()
+            .single()
+        return response.data?.character?.fragments?.characterDetail
+    }
+
+    suspend fun getEpisodes(page: Int): GetEpisodesQuery.Episodes? {
+        val response = apolloClient.query(GetEpisodesQuery(Input.optional(page)))
+            .execute()
+            .single()
+        return response.data?.episodes
+    }
+
+    suspend fun getEpisode(episodeId: String): EpisodeDetail? {
+        val response = apolloClient.query(GetEpisodeQuery(episodeId))
+            .execute()
+            .single()
+        return response.data?.episode?.fragments?.episodeDetail
+    }
+
+    suspend fun getLocations(page: Int): GetLocationsQuery.Locations? {
+        val response = apolloClient.query(GetLocationsQuery(Input.optional(page)))
+            .execute()
+            .single()
+        return response.data?.locations
+    }
+
+    suspend fun getLocation(locationId: String): LocationDetail? {
+        val response = apolloClient.query(GetLocationQuery(locationId))
+            .execute()
+            .single()
+        return response.data?.location?.fragments?.locationDetail
+    }
+
+}
---
+++ shared/src/commonMain/kotlin/com/upco/playground/mortycomposekmm/shared/Platform.kt
@@ -0,0 +1,5 @@
+package com.upco.playground.mortycomposekmm.shared
+
+expect class Platform() {
+    val platform: String
+}
